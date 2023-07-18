dd5e5742de0b0ecb13b7f094ab2db69011dc500b
JoãoYp-22003298 <joaomayp@gmail.com>
2023-07-18 20:35:50 +0100

    Initial Code Commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      greenSea: "#00DFB7",
+      grey: "#AEAEAE",
+      dark: "#212121",
+      black: "#000000",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+};
